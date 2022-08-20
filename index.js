208383a2204ab3ced932acfd72602d8ac79e701d
N N Advith <nnadvith08042001@gmail.com>
2022-08-20 15:48:07 +0530

    CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/routes.js'
+import cors from 'cors'
 
 
 const app = express(); 
@@ -8,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false })) 
 app.use("/", router)
-
+app.use(cors())
 
 
 app.listen(PORT, function () {    
